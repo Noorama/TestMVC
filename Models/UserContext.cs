77166c9dc3cbe9f5d5c54816664efbb79590a200
Noorama <maxym.sydorov@rambler.ru>
2016-02-08 23:08:26 +0200

    User Entity Created

---
+++ Models/UserContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TestMVC.Models
+{
+    class UserContext : DbContext
+    {
+        public UserContext()
+            : base("DbConnection")
+        { }
+
+
+        public DbSet<User> Users { get; set; }
+    }
+}
